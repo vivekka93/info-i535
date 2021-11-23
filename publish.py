36b5391c39602e529d1a5c69c07511729c7e47ff
Vivek Karna <vivek.ka93@gmail.com>
2021-11-22 19:35:44 -0500

    Added log generator

---
+++ publish.py
@@ -0,0 +1,43 @@
+
+from stream_logs import generate_log
+import logging
+from google.cloud import pubsub_v1
+import random
+import time
+
+
+PROJECT_ID="info-i535"
+TOPIC = "userlogs"
+
+
+publisher = pubsub_v1.PublisherClient()
+topic_path = publisher.topic_path(PROJECT_ID, TOPIC)
+
+
+
+def publish(publisher, topic, message):
+    #UTF encoding the message and publishing it to Pub/Sub topic
+    data = message.encode('utf-8')
+    return publisher.publish(topic_path, data = data)
+
+
+
+def callback(message_future):
+    # When timeout is unspecified, the exception method waits indefinitely.
+    if message_future.exception(timeout=30):
+        print('Publishing message on {} threw an Exception {}.'.format(
+            topic_name, message_future.exception()))
+    else:
+        print(message_future.result())
+
+
+if __name__ == '__main__':
+
+    while True:
+        line = generate_log()
+        print(line)
+        message_future = publish(publisher, topic_path, line)
+        message_future.add_done_callback(callback)
+
+        sleep_time = random.choice(range(1, 3, 1))
+        time.sleep(sleep_time)
